26b5708d5c2d699bd9b607ab3503ba2d93059373
oag <100293071+Chen8671@users.noreply.github.com>
2023-06-09 09:22:59 -0400

    Update orderModel.php

--- orderModel.php
+++ orderModel.php
@@ -1,12 +1,12 @@
 <?php
 $host = 'localhost';
-$dbuser ='root';//帳號
-$dbpassword = '';//密碼
-$dbname = 'final_exam';
+$dbuser ='sarah';//帳號
+$dbpassword ='00000';//密碼
+$dbname = 'vip';
 $link = mysqli_connect($host,$dbuser,$dbpassword,$dbname);
 if($link){
     mysqli_query($link,'SET NAMES utf8');
-    // echo "正確連接資料庫";
+        echo "正確連接資料庫";
 }
 else {
     echo "不正確連接資料庫</br>" . mysqli_connect_error();
